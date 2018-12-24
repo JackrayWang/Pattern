ce37308dd6984f4c5428927294d5856c312ed87e
jackray <jackray@DESKTOP-B8VEO9D>
2018-12-24 09:29:04 +0800

    策略模式

--- StrategyPattern/src/superclass/Duck.java
+++ StrategyPattern/src/superclass/Duck.java
@@ -19,4 +19,22 @@ public abstract class Duck {
 	public void getIf() {
 		ifi.fly();
 	}
+
+	public IQuackDuckInterface getIq() {
+		return iq;
+	}
+
+	public void setIq(IQuackDuckInterface iq) {
+		this.iq = iq;
+	}
+
+	public IFlyDuckInterface getIfi() {
+		return ifi;
+	}
+
+	public void setIfi(IFlyDuckInterface ifi) {
+		this.ifi = ifi;
+	}
+	
+	
 }
